d92b1e5077a10a7026947d6895a472f31a63024a
Steven Chen <stevenchen8988@gmail.com>
2023-05-30 17:10:54 -0400

    added

---
+++ ballFollow.py
@@ -0,0 +1,87 @@
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import cv2
+import imutils
+import time
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help=r"C:\Users\steve\Videos\MyComGameCenter")
+ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
+
+args = vars(ap.parse_args())
+
+# Update the color range for a tennis ball
+greenLower = (20, 65, 5)  # Lower range for yellow/greenish-yellow
+greenUpper = (64, 255, 255)  # Upper range for yellow/greenish-yellow
+
+pts = deque(maxlen=args["buffer"])
+prev_pt = None
+prediction_enabled = False
+predicted_pt = None
+
+if not args.get("video", False):
+    vs = VideoStream(src=0).start()
+else:
+    vs = cv2.VideoCapture(args["video"])
+    time.sleep(2.0)
+
+while True:
+    frame = vs.read()  # Read a frame from the video stream
+    frame = frame[1] if args.get("video", False) else frame  # For video files, select the second item
+
+    if frame is None:
+        break
+
+    frame = imutils.resize(frame, width=800)  # Adjust the width to a smaller value
+
+    blurred = cv2.GaussianBlur(frame, (15, 15), 0)
+
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+    center = None
+
+    if len(contours) > 0:
+        c = max(contours, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+
+        if radius > 10:
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+            cv2.circle(frame, (int(x), int(y)), 5, (0, 0, 255), -1)
+
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        prev_pt = center
+
+    if center is not None:
+        pts.appendleft(center)
+
+    if len(pts) >= 2:
+        for i in range(1, len(pts)):
+            if pts[i - 1] is None or pts[i] is None:
+                continue
+            thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+    # Display the frame and mask
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+if not args.get("video", False):
+    vs.stop()
+else:
+    vs.release()
+
+cv2.destroyAllWindows()
